ab41f7089c97133ca73e36d2e6e008dccbe538aa
bmbye <b_mbye@yahoo.com>
2022-07-23 17:33:47 -0400

    Prompt the user for input and run through 5 games

--- rps.js
+++ rps.js
@@ -4,25 +4,56 @@ let Choices = {
     scissors: {LosesTo: "rock"},
 }
 
+let Score = {
+    userScore: 0,
+    pcScore: 0,
+}
+
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"]
-    return choice[Math.random() * 3]
+    return choice[Math.floor(Math.random() * 3)]
 }
 
 function playARound(userChoice, compChoice) {
     const lowerChoice = userChoice.toLowerCase()
+
     switch(true) {
+        case !(userChoice in Choices):
+            return "You must choose Rock, Paper or Scissors."
         case lowerChoice == compChoice:
             return "It's a Draw!"
         case lowerChoice == Choices[compChoice].LosesTo:
+            Score.userScore++
             return `You Win! ${userChoice.toUpperCase()} beats ${compChoice.toUpperCase()}`
         case Choices[lowerChoice].LosesTo == compChoice:
+            Score.pcScore++
             return `You Lose! ${compChoice.toUpperCase()} beats ${userChoice.toUpperCase()}`
         default:
             return
     }   
 }
 
-const playerChoice = "rock"
-const pcChoice = getComputerChoice()
-console.log(playARound(playerChoice, pcChoice))
+function getWinner() {
+    switch(true) {
+        case Score.userScore == Score.pcScore:
+            return "It's a Draw!"
+        case Score.userScore > Score.pcScore:            
+            return `You won ${Score.userScore} out of 5 games, you are the winner!`
+        case Score.userScore < Score.pcScore:
+            return `You won ${Score.userScore} out of 5 games, you are the loser.`
+        default:
+            return
+    } 
+}
+
+function game() {
+    for (let i = 0;i <= 5;i++) {
+        const playerChoice = prompt("Rock, Paper or Scissors?")
+        const pcChoice = getComputerChoice()
+        console.log(playARound(playerChoice, pcChoice))
+    }
+
+    console.log(getWinner())
+}
+
+game()
