06296f833f9e1422325d93c0046d7ceeb23dc26b
bmbye <b_mbye@yahoo.com>
2022-08-02 20:54:33 -0400

    Complete the UI and functionality

--- rps.js
+++ rps.js
@@ -9,51 +9,101 @@ let Score = {
     pcScore: 0,
 }
 
+let round = 1
+
+function reset() {
+    Score.pcScore = 0;
+    Score.userScore = 0;
+    round = 1;
+    document.querySelector('.message').innerHTML = ""
+    document.querySelector('.userScore').innerHTML = `User Score: ${Score.userScore}`
+    document.querySelector('.compScore').innerHTML = `PC Score: ${Score.pcScore}`
+    document.querySelector('.message').innerHTML = ""
+    document.querySelector('.round').innerHTML = `Round ${round}`;
+    document.querySelector('.tally').innerHTML = `Tally: ${Score.userScore}-${Score.userScore}`
+}
+
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"]
     return choice[Math.floor(Math.random() * 3)]
 }
 
-function playARound(userChoice, compChoice) {
-    const lowerChoice = userChoice.toLowerCase()
-
-    switch(true) {
-        case !(userChoice in Choices):
-            return "You must choose Rock, Paper or Scissors."
-        case lowerChoice == compChoice:
-            return "It's a Draw!"
-        case lowerChoice == Choices[compChoice].LosesTo:
-            Score.userScore++
-            return `You Win! ${userChoice.toUpperCase()} beats ${compChoice.toUpperCase()}`
-        case Choices[lowerChoice].LosesTo == compChoice:
-            Score.pcScore++
-            return `You Lose! ${compChoice.toUpperCase()} beats ${userChoice.toUpperCase()}`
-        default:
-            return
-    }   
-}
-
 function getWinner() {
+    let msg  = ""
+
     switch(true) {
         case Score.userScore == Score.pcScore:
-            return "It's a Draw!"
+            msg = "It's a Draw!"
+            break
         case Score.userScore > Score.pcScore:            
-            return `You won ${Score.userScore} out of 5 games, you are the winner!`
+            msg = `You won 5 games, you are the winner!`
+            break
         case Score.userScore < Score.pcScore:
-            return `You won ${Score.userScore} out of 5 games, you are the loser.`
+            msg = `The PC won 5 games, you are the loser.`
+            break
         default:
-            return
+            break
     } 
+
+    document.querySelector('.message').innerHTML = msg
+    setTimeout(() => {
+        reset()
+    }, 5000)
 }
 
-function game() {
-    for (let i = 0;i <= 5;i++) {
-        const playerChoice = prompt("Rock, Paper or Scissors?")
-        const pcChoice = getComputerChoice()
-        console.log(playARound(playerChoice, pcChoice))
+function updateScore(winner) {
+    if (winner === "user") {
+        Score.userScore++
+        document.querySelector('.userScore').innerHTML = `User Score: ${Score.userScore}`
+    } else if (winner === "comp") {
+        Score.pcScore++
+        document.querySelector('.compScore').innerHTML = `PC Score: ${Score.pcScore}`
     }
 
-    console.log(getWinner())
+    document.querySelector('.tally').innerHTML = `Tally: ${Score.userScore}-${Score.pcScore}`
 }
 
-game()
+function playARound(e) {
+    const userChoice = e.target.getAttribute('class')
+    const compChoice = getComputerChoice()
+    const lowerChoice = userChoice.toLowerCase()
+    const messageElement = document.querySelector('.message')
+
+    switch(true) {
+        case lowerChoice == compChoice:
+            messageElement.innerHTML = "It's a Draw!"
+            break
+        case lowerChoice == Choices[compChoice].LosesTo:
+            updateScore("user")
+            messageElement.innerHTML = `You Win! ${userChoice.toUpperCase()} beats ${compChoice.toUpperCase()}`
+            break
+        case Choices[lowerChoice].LosesTo == compChoice:
+            updateScore("comp")
+            messageElement.innerHTML = `You Lose! ${compChoice.toUpperCase()} beats ${userChoice.toUpperCase()}`
+            break
+        default:
+            break
+    }   
+
+    round++;
+    document.querySelector('.round').innerHTML = `Round ${round}`;
+    if (Score.userScore === 5 || Score.pcScore === 5) {
+        getWinner()
+        return
+    }
+}
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => button.addEventListener('click', playARound));
+
+// function game() {
+//     for (let i = 0;i <= 5;i++) {
+//         const playerChoice = prompt("Rock, Paper or Scissors?")
+//         const pcChoice = getComputerChoice()
+//         console.log(playARound(playerChoice, pcChoice))
+//     }
+
+//     console.log(getWinner())
+// }
+
+// game()
